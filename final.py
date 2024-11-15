0834feafb22b8620aa6726e36af2859beabcc1d8
alejx96 <alej.gal96@gmail.com>
2024-11-14 18:10:38 -0600

    modif1

---
+++ final.py
@@ -0,0 +1,23 @@
+import functions as func
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Hipertension_Arterial_Mexico.csv')
+
+#Tratado de datos
+df = df.drop_duplicates()
+df = df.dropna()
+df = df[df['estatura'] > 0]
+dfnew = pd.DataFrame()
+dfnew['IMC'] = df['peso'] / ((df['estatura'] / 100) ** 2)
+dfnew['edad'] = df['edad'].apply(func.clasificar_edad)
+dfnew['acido_urico'] = df['valor_acido_urico'].apply(func.clasificar_acido_urico)
+dfnew['colesterol'] = df['valor_colesterol_total'].apply(func.clasificar_colesterol_total)
+dfnew['creatina'] = df.apply(lambda x: func.clasificar_creatina(x['valor_creatina'], x['sexo'], x['edad']), axis=1)
+dfnew['glucosa'] = df['resultado_glucosa'].apply(func.clasificar_glucosa)
+dfnew['trigliceridos'] = df['valor_trigliceridos'].apply(func.clasificar_trigliceridos)
+dfnew['insulina'] = df['valor_insulina'].apply(func.clasificar_insulina)
+
+print(dfnew)
+
+dfnew.to_csv('test.csv',index=False,encoding='utf-8')
---
+++ functions.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+'''Edad'''
+def clasificar_edad(edad):
+    if edad < 13:
+        return 'Niño'
+    elif 13 <= edad < 18:
+        return 'Adolescente'
+    elif 18 <= edad < 65:
+        return 'Adulto'
+    else:
+        return 'Adulto Mayor'
+
+'''acido urico'''
+def clasificar_acido_urico(valor):
+    if 3.5 <= valor <= 7:
+        return 'Normal'
+    else:
+        return 'Fuera de rango'
+    
+'''colesterol'''
+def clasificar_colesterol_total(valor):
+    if valor < 200:
+        return 'Riesgo bajo'
+    else:
+        return 'Riesgo alto'
+
+'''Creatina'''
+def clasificar_creatina(valor, sexo, edad):
+    if sexo == 2:  # Mujer
+        if 0.6 <= valor <= 1.1:
+            return 'Normal'
+    elif sexo == 1:  # Hombre
+        if 0.7 <= valor <= 1.3:
+            return 'Normal'
+    elif 13 <= edad <= 17:  # Adolescentes
+        if 0.5 <= valor <= 1.0:
+            return 'Normal'
+    elif edad < 13:  # Niños
+        if 0.3 <= valor <= 0.7:
+            return 'Normal'
+    return 'Fuera de rango'
+
+'''Glucosa'''
+def clasificar_glucosa(valor):
+    if 80 <= valor <= 130:
+        return 'Óptimo'
+    elif valor <= 180:
+        return 'Normal elevado'
+    else:
+        return 'Alto'
+
+'''Trigliceridos'''
+def clasificar_trigliceridos(valor):
+    if valor < 150:
+        return 'Normal'
+    elif valor <= 200:
+        return 'Límite alto'
+    else:
+        return 'Alto'
+    
+'''Insulina'''
+def clasificar_insulina(valor):
+    if 5 <= valor <= 25:
+        return 'Normal'
+    else:
+        return 'Fuera de rango'
