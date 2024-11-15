38c8bfc012c6e1525a51af144c955ac0512c0523
alejx96 <alej.gal96@gmail.com>
2024-11-15 10:36:08 -0600

    modif6

--- final.py
+++ final.py
@@ -207,5 +207,36 @@ plt.title('Insulina por Edad')
 plt.legend(loc='best')
 plt.show()
 
+#Presion Arterial
+dfEdad = dfnew.pivot_table(index='edad',columns='presion_arterial',aggfunc='size',fill_value=0)
 
+dfEdad = round(dfEdad.div(dfEdad.sum(axis=1),axis=0)*100,2)
+print(dfEdad)
+columns = dfEdad.columns.tolist()[0:]
+colors = ['#ff335e', '#335eff', '#4cb274','#a14cb2']
+barWidth = 0.25
+
+br1 = np.arange(dfEdad.shape[0]) - barWidth
+br2 = br1 + barWidth
+br3 = br2 + barWidth
+br4 = br3 + barWidth
+brs = [br1, br2, br3, br4]
+print(br1)
+
+plt.figure(figsize=(10,8))
+for br, color, col_name in zip(brs, colors, columns):
+    print(br,col_name)
+    plt.bar(br, dfEdad[col_name], width=barWidth, color=color, label=col_name)
+
+
+plt.xticks(br2, dfnew.groupby('edad').groups.keys())
+
+ax = plt.gca()
+for br, color, col_name in zip(brs, colors, columns):
+    for j, v in enumerate(dfEdad[col_name].values):
+        ax.text(br[j]-barWidth*0.5, v+1, f'{v:,.2f}', color=color, fontweight='bold',fontsize=8)
+
+plt.title('Presion Arterial por Edad')
+plt.legend(loc='best')
+plt.show()
 # dfnew.to_csv('test.csv',index=False,encoding='utf-8')
